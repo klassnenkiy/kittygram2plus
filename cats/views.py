51cc337b6884308a76c7fc17badb73741254a8d6
Stanislav Tyulyagin <klassnenkiy@gmail.com>
2023-03-06 03:46:31 +0300

    1

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,35 @@
-from rest_framework import viewsets
-
+from rest_framework import permissions, viewsets
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
-
+from .throttling import WorkingHoursRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',) 
+
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+    # Если в GET-запросе требуется получить информацию об объекте
+    if self.action == 'retrieve':
+        # Вернем обновленный перечень используемых пермишенов
+        return (ReadOnly(),)
+    # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+    return super().get_permissions() 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -7,9 +7,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-
 from datetime import timedelta
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'djoser',
     'cats.apps.CatsConfig',
 ]
@@ -134,6 +134,18 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '10000/day',  # Лимит для UserRateThrottle
+        'anon': '1000/day',  # Лимит для AnonRateThrottle
+        'low_request': '1/minute',
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 SIMPLE_JWT = {
